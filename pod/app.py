70f9602cdeb9854fd7ae60e1698a575293aadac5
Jedddy <jedbalita25@gmail.com>
2023-06-08 22:45:06 +0800

    feat: add cursor states

--- pod/app.py
+++ pod/app.py
@@ -11,15 +11,53 @@ ENTER = (curses.KEY_ENTER, ord('\n'), ord('\r'))
 
 
 class App:
-    selected: str
+    """The main application for displaying folders and selecting directories.
+
+    Attributes
+    ----------
+    cursor:
+        The cursor to use to point to a currently selected folder or file.
+
+    keep_cursor_state:
+        Whether to keep the cursor state when entering/leaving a directory.
+
+    idx:
+        The index of the cursor on the list of entities.
+
+    offset:
+        The offset of the display screen from the top of the file.
+
+    screen_idx:
+        The index of the cursor on the screen.
+
+    cwd:
+        The current working directory.
+
+    cursor_stack:
+        The cursor stack used to 'remember' the past cursor placement before
+        entering a new directory. It is a tuple of 4 items.
+        (part, offset, index, screen_index)
+
+        `part` is the folder name.
+        `offset` is the offset of the display screen from the top of the file list.
+        `index` is the index of the cursor.
+        `screen index` is the index of the cursor on the screen
+
+    files:
+        The list of files on the current working directory.
+    """
+
     screen: "curses._CursesWindow"
 
-    def __init__(self, cursor: str):
+    def __init__(self, cursor: str, keep_cursor_state: bool):
         self.cursor = cursor.strip()
+        self.keep_cursor_state = keep_cursor_state
         self.idx = 0
         self.offset = 0
         self.screen_idx = 0
         self.cwd = Path(os.getcwd())
+        self.cursor_stack = [(part, 0, 0, 0) for part in self.cwd.parts]
+        self.files = self.get_files()
 
     def __call__(self, screen: "curses._CursesWindow"):
         curses.curs_set(0)
@@ -33,6 +71,11 @@ class App:
             self.screen.addstr(1, 1, "Press any key to exit.")
             self.screen.refresh()
             self.screen.getch()
+
+    def reset_state(self) -> None:
+        self.idx = 0
+        self.offset = 0
+        self.screen_idx = 0
 
     def get_files(self) -> tuple[tuple[int, Back | Path], ...]:
         back = [Back()]
@@ -66,7 +109,6 @@ class App:
             y += 1
 
             if self.idx == idx:
-                self.selected = file.name if not isinstance(file, Back) else str(file)
                 file_display = f"{self.cursor} {file}"
             else:
                 file_display = f"{' ' * len(self.cursor)} {file}"
@@ -89,7 +131,7 @@ class App:
             self.screen_idx -= 1
 
     def move_down(self) -> None:
-        if self.idx < len(self.get_files()) - 1:
+        if self.idx < len(self.files) - 1:
             self.idx += 1
 
             if self.screen_idx >= self.maxy - 1:
@@ -97,6 +139,11 @@ class App:
 
             if self.screen_idx < self.maxy - 1:
                 self.screen_idx += 1
+
+    def pop_cursor_stack(self):
+        if len(self.cursor_stack) > 1:
+            return self.cursor_stack.pop()
+        return self.cursor_stack[0]
 
     def _run(self):
         while True:
@@ -113,15 +160,35 @@ class App:
             if key in ENTER:
                 if self.idx == 0:
                     selected = self.cwd.parent
+
+                    os.chdir(selected)
+
+                    if self.keep_cursor_state:
+                        _, offset, index, screen_index = self.pop_cursor_stack()
+
+                        self.offset = offset
+                        self.idx = index
+                        self.screen_idx = screen_index
+
                 else:
-                    selected = self.cwd / self.selected
+                    selected = self.cwd / str(self.files[self.idx][1].name)
 
                     if selected.is_file():
                         continue
 
-                os.chdir(selected)
+                    if self.keep_cursor_state:
+                        state = (
+                            self.cwd.name,
+                            self.offset,
+                            self.idx,
+                            self.screen_idx
+                        )
+
+                        self.cursor_stack.append(state)
+
+                    os.chdir(selected)
+
+                    self.reset_state()
 
                 self.cwd = Path(os.getcwd())
-                self.idx = 0
-                self.offset = 0
-                self.screen_idx = 0
+                self.files = self.get_files()
