eff66139c366cc765db30cd9d7e9b73a80ee7c20
Jedddy <jedbalita25@gmail.com>
2023-06-08 23:07:51 +0800

    chore: made a separate file for getting args

---
+++ pod/options.py
@@ -0,0 +1,24 @@
+import argparse
+from typing import Sequence
+
+
+def get_args(argv: Sequence[str]):
+    parser = argparse.ArgumentParser(
+        prog="pod",
+        allow_abbrev=False
+    )
+
+    parser.add_argument(
+        "--cursor",
+        default="=>",
+        help="The cursor to use."
+    )
+
+    parser.add_argument(
+        "--no-keep-state",
+        default=False,
+        help="Whether not to keep the cursor state after entering/leaving a directory.",
+        action="store_true"
+    )
+
+    return parser.parse_known_args(argv)
